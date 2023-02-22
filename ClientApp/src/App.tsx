03fdcfcf670870a0ae038e234d409ba056f0b626
rithikagangishetty <121901013@smail.iitpkd.ac.in>
2023-02-22 17:34:25 +0530

    Initial Commit

--- ClientApp/src/App.tsx
+++ ClientApp/src/App.tsx
@@ -4,13 +4,21 @@ import Layout from './components/Layout';
 import Home from './components/Home';
 import Counter from './components/Counter';
 import FetchData from './components/FetchData';
+import UserAPI from './components/UserAPI'
 
 import './custom.css'
 
-export default () => (
-    <Layout>
-        <Route exact path='/' component={Home} />
-        <Route path='/counter' component={Counter} />
-        <Route path='/fetch-data/:startDateIndex?' component={FetchData} />
-    </Layout>
-);
+//export default () => (
+//    <Layout>
+//        <Route exact path='/' component={Home} />
+//        <Route path='/counter' component={Counter} />
+//        <Route path='/fetch-data/:startDateIndex?' component={FetchData} />
+//    </Layout>
+//);
+function App() {
+    return 
+    (
+        <UserAPI />
+        );
+}
+export default App;
